22ceda205c3862684487ccfb53227b6111c6e67c
Wartets <colin.reaubourg@gmail.com>
2025-02-27 17:21:10 +0100

    new lists

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const sources = [
     { url: "https://raw.githubusercontent.com/Wartets/RandomWord/refs/heads/main/lists/insultes.txt", name: "Insultes", enabled: false },
     { url: "https://raw.githubusercontent.com/Wartets/RandomWord/refs/heads/main/lists/nosense.txt", name: "Aucun Sens", enabled: false },
     { url: "https://raw.githubusercontent.com/Wartets/RandomWord/refs/heads/main/lists/wakfu.txt", name: "Wakfu", enabled: false },
-    { url: "https://raw.githubusercontent.com/Wartets/RandomWord/refs/heads/main/lists/physique.txt", name: "Physique", enabled: false },
+    { url: "https://raw.githubusercontent.com/Wartets/RandomWord/refs/heads/main/lists/science.txt", name: "Science", enabled: false },
     { url: "https://raw.githubusercontent.com/kuel27/wordlist/main/wordlist.txt", name: "Kuel27 (en)", enabled: false }
 ];
 
